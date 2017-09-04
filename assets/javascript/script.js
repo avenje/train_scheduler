5e574ceab7661deccf4714a26d0993aca32e0aa6
Jeremy Avenarius <jeremy@Jeremys-MacBook-Air.local>
2017-09-04 16:30:22 -0600

    fixed minutes to arrival bug, final push

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,80 +1,91 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCEpaBZSXnZnx_-umoXagNR-FoDBEluXkg",
-    authDomain: "eurorail-schedule.firebaseapp.com",
-    databaseURL: "https://eurorail-schedule.firebaseio.com",
-    projectId: "eurorail-schedule",
-    storageBucket: "eurorail-schedule.appspot.com",
-    messagingSenderId: "614693859028"
-  };
-  firebase.initializeApp(config);
+$(document).ready(function() {
+      // Initialize Firebase
+      var currentTime = new moment ().format("HH:mm");
 
+      $("#currentTime").text("Current time is:  " + currentTime);
 
-  var database = firebase.database();
-// 2. Button for adding trains
-$("#add-train-btn").on("click", function(event) {
-  event.preventDefault();
-  // Grabs user input
-  var trainName = $("#train-name-input").val().trim();
-  var dest = $("#destination-input").val().trim();
-  var trainStart = $("#start-input").val().trim();
-  var trainRate = $("#rate-input").val().trim();
-  // Creates local "temporary" object for holding train data
-  var newTrain = {
-    name: trainName,
-    destination: dest,
-    start: trainStart,
-    rate: trainRate
-  };
+      var timeEntered = $('.timeEntered').val();
 
-  // Uploads train data to the database
-  database.ref().push(newTrain);
-  // Logs everything to console
-  console.log(newTrain.name);
-  console.log(newTrain.destination);
-  console.log(newTrain.start);
-  console.log(newTrain.rate);
-  // Alert
-  alert("train successfully added");
-  // Clears all of the text-boxes
-  $("#train-name-input").val("");
-  $("#destination-input").val("");
-  $("#start-input").val("");
-  $("#rate-input").val("");
-});
+      if (!moment(timeEntered,'HH:mm').isValid()) {
+        console.log('Invalid Date');
+      } else {
+        console.log('Valid Date');
+      }
 
-// 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+      var config = {
+        apiKey: "AIzaSyCEpaBZSXnZnx_-umoXagNR-FoDBEluXkg",
+        authDomain: "eurorail-schedule.firebaseapp.com",
+        databaseURL: "https://eurorail-schedule.firebaseio.com",
+        projectId: "eurorail-schedule",
+        storageBucket: "eurorail-schedule.appspot.com",
+        messagingSenderId: "614693859028"
+      };
+
+      firebase.initializeApp(config);
+
+      var database = firebase.database();
+
+    // Button for adding trains
+    $("#add-train-btn").on("click", function(event) {
+      event.preventDefault();
+
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var dest = $("#destination-input").val().trim();
+    var trainStart = $("#start-input").val().trim();
+    var trainRate = $("#rate-input").val().trim();
+    var firstTimeConverted = moment(trainStart, "HH:mm").subtract(1, "years");
+    var currentTime = moment();
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    var tRemainder = diffTime % trainRate;
+    var minutesTillTrain = trainRate - tRemainder;
+    var NextArrival = moment().add(minutesTillTrain, "minutes");
+    var minutesAway = moment(NextArrival).format("HH:mm");
+
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+      name: trainName,
+      destination: dest,
+      start: trainStart,
+      rate: trainRate,
+      minutesAway: minutesAway,
+      minutesTillTrain: minutesTillTrain
+    };
+
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#start-input").val("");
+    $("#rate-input").val("");
+  });
+
+  // 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+    console.log(childSnapshot.val());
 
   // Store everything into a variable.
   var trainName = childSnapshot.val().name;
   var dest = childSnapshot.val().destination;
-  // var trainStart = childSnapshot.val().start;
+  var trainStart = childSnapshot.val().start;
   var trainRate = childSnapshot.val().rate;
-  // var tRate= childSnapshot.val().rate;
-    // find when the next train is and minutes until next train
-  var tRate = childSnapshot.val().frequency;
-  // pushed back 1 year to make sure it comes before current time
-  var convertedDate = moment(childSnapshot.val().trainName, 'hh:mm');
-  var trainTime = moment(convertedDate).format('HH:mm');
+  // Hardcore math using moment.js
+  var firstTimeConverted = moment(trainStart, "HH:mm").subtract(1, "years");
   var currentTime = moment();
-  // pushed back 1 year to make sure it comes before current time
-  var firstTimeConverted = moment(trainTime,'hh:mm');
   var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-  var tRemainder = diffTime % tRate;
-  //solved
-  var tMinutesTillTrain = tRate - tRemainder;
-  //solved
-  var nextTrain = moment().add(tMinutesTillTrain, 'minutes').format('HH:mm')
+  var tRemainder = diffTime % trainRate;
+  var minutesTillTrain = trainRate - tRemainder;
+  var NextArrival = moment().add(minutesTillTrain, "minutes");
+  var minutesAway = moment(NextArrival).format("HH:mm");
 
-  
-  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + dest + "</td><td>" +
-   trainRate + "</td><td>" + nextTrain + "</td><td>" + tMinutesTillTrain + "</td><td>");
- },function(errorObject) {
-    console.log('Errors handled: ' + errorObject.code);
-  })
+  // Add data to table
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + dest + "</td><td>" + trainRate + "</td><td>" + minutesAway + "</td><td>" + minutesTillTrain + "</td><td>");
+});
 
-//refreashes train data every minute
-setInterval(function(){
+  //Refreshes train data every minute
+  setInterval(function(){
     location.reload();
   }, 60000)
+});
